116c3bf57723e36423773be5656b24b673884c23
Prashanna69 <plohani2003@gmail.com>
2024-01-18 19:47:26 +0545

    Added Feature and upadate minor bugs

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,4 +1,17 @@
-import { Box, Button, Flex, Spacer, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Flex,
+  Heading,
+  Image,
+  SimpleGrid,
+  Spacer,
+  Text,
+} from "@chakra-ui/react";
 export default function Home() {
   return (
     <>
@@ -34,13 +47,55 @@ export default function Home() {
           Get started
         </Button>
       </Flex>
-      <Box backgroundColor="teal">
-        <Flex alignItems="center">
-          <Text fontSize="4xl" as="b">
-            Features
-          </Text>
-        </Flex>
-      </Box>
+      <Flex height="100vh" justifyContent="center">
+        <Box minH="auto" borderRadius="1rem">
+          <Flex justifyContent="center">
+            <Text fontSize="4xl" as="b" mt="4rem">
+              Features
+            </Text>
+          </Flex>
+          <Flex
+            maxW="1200px"
+            p="2rem"
+            justifyContent="space-between"
+            gap="1rem"
+          >
+            <Card>
+              <Image src="./Image/Feature1.jpg" maxW="22rem" />
+              <CardBody>
+                <Text>
+                  View a summary of all your customers over the last month.
+                </Text>
+              </CardBody>
+              <CardFooter>
+                <Button>View here</Button>
+              </CardFooter>
+            </Card>
+            <Card>
+              <Image src="./Image/Feature1.jpg" maxW="22rem" />
+              <CardBody>
+                <Text>
+                  View a summary of all your customers over the last month.
+                </Text>
+              </CardBody>
+              <CardFooter>
+                <Button>View here</Button>
+              </CardFooter>
+            </Card>
+            <Card>
+              <Image src="./Image/Feature1.jpg" maxW="22rem" />
+              <CardBody>
+                <Text>
+                  View a summary of all your customers over the last month.
+                </Text>
+              </CardBody>
+              <CardFooter>
+                <Button>View here</Button>
+              </CardFooter>
+            </Card>
+          </Flex>
+        </Box>
+      </Flex>
       <footer>
         <Flex gap="1.5rem" justifyContent="center" flexWrap="wrap" mt="2rem">
           <Text>Contact: plohani2003@gmail.com</Text>
