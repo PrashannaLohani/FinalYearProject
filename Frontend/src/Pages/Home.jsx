4cb46e6813654c9317a9c22ee006ea01ffac24f0
Prashanna69 <plohani2003@gmail.com>
2024-01-27 20:24:29 +0545

    Upfate in Homepage with different image and content

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -5,13 +5,19 @@ import {
   CardBody,
   CardFooter,
   CardHeader,
+  CheckboxIcon,
   Flex,
   Heading,
   Image,
+  List,
+  ListIcon,
+  ListItem,
+  OrderedList,
   SimpleGrid,
   Spacer,
   Text,
 } from "@chakra-ui/react";
+import { FaCircle } from "react-icons/fa6";
 export default function Home() {
   return (
     <>
@@ -51,53 +57,123 @@ export default function Home() {
         <Box minH="auto" borderRadius="1rem">
           <Flex justifyContent="center">
             <Text fontSize="4xl" as="b" mt="4rem">
-              Features
+              Achieve Sucess with React&Rise
             </Text>
           </Flex>
-          <Flex
-            maxW="1200px"
-            p="2rem"
-            justifyContent="space-between"
-            gap="1rem"
+
+          <SimpleGrid
+            minChildWidth="400px"
+            column={3}
+            spacing="2"
+            padding="4rem"
           >
             <Card>
-              <Image src="./Image/Feature1.jpg" maxW="22rem" />
+              <Image src="./Image/Feature1.jpg" borderRadius="8" />
+              <CardHeader>
+                <Heading size="lg">Work</Heading>
+              </CardHeader>
               <CardBody>
                 <Text>
-                  View a summary of all your customers over the last month.
+                  Enhance the productivity of your work-related meetings by
+                  collecting valuable input, feedback, and insights from your
+                  colleagues. Collaborate to make more informed decisions as a
+                  team!
                 </Text>
               </CardBody>
               <CardFooter>
-                <Button>View here</Button>
+                <Button>Read more</Button>
               </CardFooter>
             </Card>
             <Card>
-              <Image src="./Image/Feature1.jpg" maxW="22rem" />
+              <Image src="./Image/Education.jpg" borderRadius="8" />
+              <CardHeader>
+                <Heading size="lg">Education</Heading>
+              </CardHeader>
               <CardBody>
                 <Text>
-                  View a summary of all your customers over the last month.
+                  Improve educational results and actively involve each student
+                  in the classroom. Explore contemporary teaching approaches by
+                  utilizing the features provided by React&Rise!
                 </Text>
               </CardBody>
               <CardFooter>
-                <Button>View here</Button>
+                <Button>Read more</Button>
               </CardFooter>
             </Card>
             <Card>
-              <Image src="./Image/Feature1.jpg" maxW="22rem" />
+              <Image src="./Image/Engagement.jpg" borderRadius="8" />
+              <CardHeader>
+                <Heading size="lg">Engagement</Heading>
+              </CardHeader>
               <CardBody>
                 <Text>
-                  View a summary of all your customers over the last month.
+                  Elevate the level of participation and captivate every
+                  audience during your presentations using React&Rise. Embrace
+                  innovative features to foster engagement and elevate the
+                  overall interactive experience for your audience.
                 </Text>
               </CardBody>
               <CardFooter>
-                <Button>View here</Button>
+                <Button>Read more</Button>
               </CardFooter>
             </Card>
-          </Flex>
+          </SimpleGrid>
         </Box>
       </Flex>
+      <SimpleGrid minChildWidth="400px" spacing="2" mt="3rem" padding="2rem">
+        <Box padding="3rem">
+          <Heading mt="3rem" maxW="36rem">
+            Transform challenging audiences into actively involved participants.
+          </Heading>
+          <List mt="2rem">
+            <OrderedList spacing="2rem">
+              <ListItem>
+                Establish an instant rapport with your audience and involve them
+                in your presentation to create a meaningful connection.
+              </ListItem>
+              <ListItem>
+                Establish an instant rapport with your audience and involve them
+                in your presentation to create a meaningful connection.
+              </ListItem>
+            </OrderedList>
+          </List>
+        </Box>
+        <Box>
+          <Image src="./Image/Crowd.jpg" />
+        </Box>
+      </SimpleGrid>
+      <SimpleGrid minChildWidth="400px" spacing="2" padding="2rem">
+        <Box>
+          <Image src="./Image/presentation.jpg" />
+        </Box>
+        <Box padding="3rem">
+          <Heading mt="3rem" maxW="36rem">
+            Craft presentations that are genuinely distinctive.
+          </Heading>
+          <List mt="2rem">
+            <OrderedList spacing="2rem">
+              <ListItem>
+                Utilize a diverse set of interactive slides to ask questions,
+                collect feedback, and engage with your audience.
+              </ListItem>
+              <ListItem>
+                Create a compelling presentation in a matter of minutes,
+                generate a meaningful icebreaker on the spot, or discover
+                innovative ways to captivate and involve your audience!
+              </ListItem>
+            </OrderedList>
+          </List>
+        </Box>
+      </SimpleGrid>
+      {/* <Box bgImage="./Image/BG.jpg" bgRepeat="no-repeat" bgSize="cover">
+        <Flex alignItems="center" height="10rem">
+          <Text fontSize="4xl">
+            Dazzle your audience with engaging and interactive presentations.
+          </Text>
+        </Flex>
+      </Box> */}
       <footer>
-        <Flex gap="1.5rem" justifyContent="center" flexWrap="wrap" mt="2rem">
+        <Flex gap="1.5rem" justifyContent="center" flexWrap="wrap" m="2rem">
           <Text>Contact: plohani2003@gmail.com</Text>
           <Text>Privacy policy</Text>
           <Text>Terms of Service</Text>
