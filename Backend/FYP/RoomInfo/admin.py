ea994db2d9a17258c66e407693674b03e9a72e88
Prashanna69 <plohani2003@gmail.com>
2024-04-02 17:10:15 +0545

    Comments Model created

--- Backend/FYP/RoomInfo/admin.py
+++ Backend/FYP/RoomInfo/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from .models import Room
+from .models import Comments
 
 class RoomAdmin(admin.ModelAdmin):
     list_display = ('room_id', 'room_name', 'num_of_people', 'num_of_comments', 'created_at','created_by')
     search_fields = ('room_id', 'room_name')  # Enable searching by these fields
 
 admin.site.register(Room, RoomAdmin)
+
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ('room', 'no_of_comment', 'comments','upvoted_comments')
+    search_fields = ('room','comments','upvoted_comments',)
+
+admin.site.register(Comments, CommentsAdmin) 
--- Backend/FYP/RoomInfo/models.py
+++ Backend/FYP/RoomInfo/models.py
@@ -1,13 +1,25 @@
 from django.db import models
 from CRM.models import Signup
 
+
+
 class Room(models.Model):
     room_id = models.CharField(max_length=100)
     room_name = models.CharField(max_length=255)
-    num_of_people = models.IntegerField()
-    num_of_comments = models.IntegerField()
+    num_of_people = models.IntegerField(default=0)
+    num_of_comments = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(Signup, on_delete = models.CASCADE, default = None)
 
     def __str__(self):
         return self.room_name
+    
+class Comments(models.Model):
+    room = models.ForeignKey('RoomInfo.Room', on_delete=models.CASCADE, default= None)
+    no_of_comment = models.IntegerField(default=0)
+    comments = models.CharField(max_length = 255)
+    no_upvoted_comments = models.IntegerField(default=0)
+    upvoted_comments = models.CharField(max_length = 225, default ='')
+
+    def __str__(self):
+        return f"Comment on Room {self.room_id}"
